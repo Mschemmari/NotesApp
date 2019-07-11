93cb912c7c98cc6335f988ddbd0e1854561c8d79
Mariano Schemmari <marianosche@gmail.com>
2019-07-11 16:20:20 -0300

    aws amplify / login / routs

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-2",
+        BUCKET: "notes-app123456"
+    },
+    apiGateway: {
+        REGION: "us-east-2",
+        URL: "https://tgxg2k67d6.execute-api.us-east-2.amazonaws.com/prod/"
+    },
+    cognito: {
+        REGION: "us-east-2",
+        USER_POOL_ID: "us-east-2_9Ag42zZ37",
+        APP_CLIENT_ID: "6nvodgm24rrl984o0isjcnn69e",
+        IDENTITY_POOL_ID: "us-east-2_9Ag42zZ37"
+    }
+};
--- src/modules/Routes.js
+++ src/modules/Routes.js
@@ -2,10 +2,13 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./home/home";
 import NotFound from "./404/404";
+import Login from "./Login/Login";
 
 
 export default () =>
     <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/login" exact component={Login} />
+
         <Route component={NotFound} />
     </Switch>;
